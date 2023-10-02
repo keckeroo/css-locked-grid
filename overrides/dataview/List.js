26b74279834df4aff7ee274554b4a2daa897c18c
Kevin Cassidy <kevincassidy@Kevins-MacBook-Pro-2.local>
2023-10-02 12:44:06 -0500

    General cleanup before working on fixes

--- overrides/dataview/List.js
+++
@@ -1,9 +0,0 @@
-Ext.define('cssLockedGrid.dataview.List', {
-    override: 'Ext.dataview.List',
-
-    privates: {
-        syncRows: function(bottomUp) {
-            this.callParent(arguments);
-        }
-    }
-})
